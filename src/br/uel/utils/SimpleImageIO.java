fd9abb54f14401183e60fc83b4aa55058a62b506
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-05-01 22:06:23 -0300

    Leitura e escrita da imagem em disco

---
+++ src/br/uel/utils/SimpleImageIO.java
@@ -0,0 +1,30 @@
+package src.br.uel.utils;
+
+import src.br.uel.image.SimpleColorImage;
+import src.br.uel.image.SimpleImage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class SimpleImageIO {
+
+    public static SimpleImage open(String path){
+        try {
+            return new SimpleColorImage(ImageIO.read(new File(path)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void save(SimpleImage image, String path){
+        try{
+            new File(path).mkdir();
+            ImageIO.write(image.getBufferedImage(), "jpg", new File(path));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
