3b511581ed73ab52c3e2476415e069cbcefede12
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-05-19 12:39:23 -0300

    criação de Look Up Table

---
+++ src/br.uel/image/process/LUT.java
@@ -0,0 +1,22 @@
+package src.br.uel.image.process;
+
+import src.br.uel.image.SimpleGrayImage;
+import src.br.uel.image.SimpleImage;
+
+public class LUT {
+
+    public SimpleImage applay(SimpleImage image, int table[]){
+        int w = image.getWidth(), h = image.getHeight();
+
+        SimpleImage imageOut = new SimpleGrayImage(w, h);
+
+        for(int y = 0; y < h; y++){
+            for(int x = 0; x < w; x++){
+                imageOut.putPixel(x,y, table[image.getPixel(x,y)]);
+            }
+        }
+
+        return imageOut;
+    }
+
+}
