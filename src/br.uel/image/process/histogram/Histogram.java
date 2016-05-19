a7f4e652e5e73e2a7fc7dea73417f97583202a4d
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-05-18 22:53:26 -0300

    cria a imagem do histograma

--- src/br.uel/image/process/histogram/Histogram.java
+++ src/br.uel/image/process/histogram/Histogram.java
@@ -1,5 +1,6 @@
 package src.br.uel.image.process.histogram;
 
+import src.br.uel.image.SimpleGrayImage;
 import src.br.uel.image.SimpleImage;
 
 public class Histogram {
@@ -18,6 +19,68 @@ public class Histogram {
         }
 
         return hist;
+    }
+
+    public int[] getNormalizedHistogram(SimpleImage image){
+        if( !image.isGray() && !image.isBinary()){
+            throw new IllegalArgumentException("The image must be a SimpleGrayImage or a SimpleBinaryImage");
+        }
+
+        int w = image.getWidth(), h = image.getHeight();
+        int total = w*h;
+        int hist[] = new int[256];
+
+        for(int y = 0; y < h; y++){
+            for(int x = 0; x < w; x++){
+                hist[image.getPixel(x,y)] += 1;
+            }
+        }
+
+        for(int i = 0; i < 256; i++){
+            hist[i] = hist[i]/total;
+        }
+
+        return hist;
+    }
+
+    public int[] getEqualizedHistogram(SimpleImage image){
+        if( !image.isGray() && !image.isBinary()){
+            throw new IllegalArgumentException("The image must be a SimpleGrayImage or a SimpleBinaryImage");
+        }
+
+        int w = image.getWidth(), h = image.getHeight();
+        int hist[] = getHistogram(image);
+        int table[] = new int[256];
+
+        int sum = 0;
+        int total = w*h;
+        for (int i = 0; i < 256; i++) {
+            sum += hist[i];
+            table[i] = Math.round(sum*256.F/total);
+        }
+
+        return table;
+    }
+
+    public SimpleImage toImage(int hist[]){
+
+        int h = 100;
+        SimpleImage imageOut = new SimpleGrayImage(256, h);
+        int max = 0;
+        for(int i = 0; i < 256; i++){
+            if(hist[i] > max){
+                max = hist[i];
+            }
+        }
+
+        float f = 1.F*max/h;
+        for(int i = 0; i < 256; i++){
+            for(int y = 0; y < hist[i]/f; y++){
+                imageOut.putPixel(i, h-y-1, 255);
+            }
+        }
+
+        return imageOut;
     }
 
 }
