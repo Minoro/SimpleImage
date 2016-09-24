81dd4b13fa84e4d69f5e11a0158c2acc7179e6d6
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-09-24 16:25:07 -0300

    Mean Filter for RGB images

--- src/br.uel/image/process/filter/MeanFilter.java
+++ src/br.uel/image/process/filter/MeanFilter.java
@@ -1,11 +1,16 @@
 package src.br.uel.image.process.filter;
 
+import src.br.uel.image.SimpleColorImage;
 import src.br.uel.image.SimpleGrayImage;
 import src.br.uel.image.SimpleImage;
 
 public class MeanFilter implements Filter {
     @Override
     public SimpleImage filter(SimpleImage image, int radius){
+
+        if(!image.isGray()){
+            return filterColorImage(image, radius);
+        }
 
         int w = image.getWidth(), h = image.getHeight();
         SimpleImage imageOut = new SimpleGrayImage(w, h);
@@ -27,6 +32,41 @@ public class MeanFilter implements Filter {
                 }
                 sum = sum/numPixels;
                 imageOut.putPixel(x, y, sum);
+            }
+        }
+
+        return imageOut;
+    }
+
+    private SimpleImage filterColorImage(SimpleImage image, int radius){
+        int w = image.getWidth(), h = image.getHeight();
+        SimpleColorImage imageOut = new SimpleColorImage(w, h);
+
+        int sumR, sumG, sumB, numPixels;
+        for(int y = 0; y < h; y++){
+            for(int x = 0; x < w; x++){
+
+                sumR = 0;
+                sumG = 0;
+                sumB = 0;
+                numPixels = 0; //don't count out of bounds pixels
+                for(int i = -radius; i <= radius; i++){
+                    for(int j = -radius; j <= radius; j++){
+                        if(x+j < 0 || x+j > w-1 || y+i < 0 || y+i > h-1){
+                            continue;
+                        }
+                        numPixels++;
+                        int rgb[] = image.getRGB(x+j,y+i);
+                        sumR += rgb[0];
+                        sumG += rgb[1];
+                        sumB += rgb[2];
+                    }
+                }
+                sumR = sumR/numPixels;
+                sumG = sumG/numPixels;
+                sumB = sumB/numPixels;
+
+                imageOut.setRGB(x, y, sumR, sumG, sumB);
             }
         }
 
