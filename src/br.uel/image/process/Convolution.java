b1edc8a4a27408799e29a20c891ff680b3a2e5f9
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-10-12 11:29:18 -0300

    Ajusta arredondamento dos canais

--- src/br.uel/image/process/Convolution.java
+++ src/br.uel/image/process/Convolution.java
@@ -18,15 +18,16 @@ public class Convolution {
 
         SimpleImage imageOut = new SimpleGrayImage(widthImage, heightImage);
 
-        for (int y = 1; y < heightImage-1; y++) {
-            for (int x = 1; x < widthImage-1; x++) {
-                int color = 0;
+        for (int y = 0; y < heightImage; y++) {
+            for (int x = 0; x < widthImage; x++) {
+//                int color = image.getPixel(x,y);
+                double color = 0;
                 for (int i = -radius; i <=radius; i++) {
                     for (int j = -radius; j <= radius; j++) {
                         if(x+j < 0 || x+j > widthImage-1 || y+i < 0 || y+i > heightImage-1){
                             continue;
                         }
-                        color += (int)(image.getPixel(x+j,y+i)*kernel[radius+i][radius+j]);
+                        color += (image.getPixel(x+j,y+i)*kernel[radius+i][radius+j]);
 
                     }
                 }
@@ -35,7 +36,7 @@ public class Convolution {
                 }else if(color < 0 ){
                     color = 0;
                 }
-                imageOut.putPixel(x,y,color);
+                imageOut.putPixel(x,y,(int)Math.round(color));
             }
         }
         return imageOut;
